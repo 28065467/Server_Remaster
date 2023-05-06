d3b666eac8fc0a82cf74474796b70fa1dddaab10
yumi0507 <michellechang958@gmail.com>
2023-05-07 01:23:28 +0800

    Add Icon

--- Server_Remaster/Form1.Designer.cs
+++ Server_Remaster/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btn_ServerStart = new System.Windows.Forms.Button();
             this.btn_GameStart = new System.Windows.Forms.Button();
             this.btn_EXIT = new System.Windows.Forms.Button();
@@ -43,9 +44,10 @@
             // 
             // btn_ServerStart
             // 
-            this.btn_ServerStart.Location = new System.Drawing.Point(12, 54);
+            this.btn_ServerStart.Location = new System.Drawing.Point(26, 108);
+            this.btn_ServerStart.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btn_ServerStart.Name = "btn_ServerStart";
-            this.btn_ServerStart.Size = new System.Drawing.Size(96, 44);
+            this.btn_ServerStart.Size = new System.Drawing.Size(208, 88);
             this.btn_ServerStart.TabIndex = 0;
             this.btn_ServerStart.Text = "Server Start";
             this.btn_ServerStart.UseVisualStyleBackColor = true;
@@ -53,9 +55,10 @@
             // 
             // btn_GameStart
             // 
-            this.btn_GameStart.Location = new System.Drawing.Point(127, 54);
+            this.btn_GameStart.Location = new System.Drawing.Point(275, 108);
+            this.btn_GameStart.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btn_GameStart.Name = "btn_GameStart";
-            this.btn_GameStart.Size = new System.Drawing.Size(96, 44);
+            this.btn_GameStart.Size = new System.Drawing.Size(208, 88);
             this.btn_GameStart.TabIndex = 1;
             this.btn_GameStart.Text = "Game Start";
             this.btn_GameStart.UseVisualStyleBackColor = true;
@@ -63,9 +66,10 @@
             // 
             // btn_EXIT
             // 
-            this.btn_EXIT.Location = new System.Drawing.Point(241, 54);
+            this.btn_EXIT.Location = new System.Drawing.Point(522, 108);
+            this.btn_EXIT.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btn_EXIT.Name = "btn_EXIT";
-            this.btn_EXIT.Size = new System.Drawing.Size(96, 44);
+            this.btn_EXIT.Size = new System.Drawing.Size(208, 88);
             this.btn_EXIT.TabIndex = 2;
             this.btn_EXIT.Text = "EXIT";
             this.btn_EXIT.UseVisualStyleBackColor = true;
@@ -75,9 +79,10 @@
             // 
             this.IP.AutoSize = true;
             this.IP.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.IP.Location = new System.Drawing.Point(465, 54);
+            this.IP.Location = new System.Drawing.Point(1008, 108);
+            this.IP.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.IP.Name = "IP";
-            this.IP.Size = new System.Drawing.Size(78, 19);
+            this.IP.Size = new System.Drawing.Size(154, 37);
             this.IP.TabIndex = 3;
             this.IP.Text = "Server IP";
             // 
@@ -85,69 +90,76 @@
             // 
             this.PORT.AutoSize = true;
             this.PORT.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PORT.Location = new System.Drawing.Point(435, 91);
+            this.PORT.Location = new System.Drawing.Point(943, 182);
+            this.PORT.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.PORT.Name = "PORT";
-            this.PORT.Size = new System.Drawing.Size(108, 19);
+            this.PORT.Size = new System.Drawing.Size(212, 37);
             this.PORT.TabIndex = 4;
             this.PORT.Text = "Server PORT";
             // 
             // tbx_IP
             // 
-            this.tbx_IP.Location = new System.Drawing.Point(565, 51);
+            this.tbx_IP.Location = new System.Drawing.Point(1224, 102);
+            this.tbx_IP.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tbx_IP.Name = "tbx_IP";
-            this.tbx_IP.Size = new System.Drawing.Size(100, 22);
+            this.tbx_IP.Size = new System.Drawing.Size(212, 36);
             this.tbx_IP.TabIndex = 5;
             // 
             // tbx_PORT
             // 
-            this.tbx_PORT.Location = new System.Drawing.Point(565, 88);
+            this.tbx_PORT.Location = new System.Drawing.Point(1224, 176);
+            this.tbx_PORT.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tbx_PORT.Name = "tbx_PORT";
-            this.tbx_PORT.Size = new System.Drawing.Size(100, 22);
+            this.tbx_PORT.Size = new System.Drawing.Size(212, 36);
             this.tbx_PORT.TabIndex = 6;
             // 
             // list_LOG
             // 
             this.list_LOG.FormattingEnabled = true;
-            this.list_LOG.ItemHeight = 12;
-            this.list_LOG.Location = new System.Drawing.Point(12, 207);
+            this.list_LOG.ItemHeight = 24;
+            this.list_LOG.Location = new System.Drawing.Point(26, 414);
+            this.list_LOG.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.list_LOG.Name = "list_LOG";
-            this.list_LOG.Size = new System.Drawing.Size(371, 232);
+            this.list_LOG.Size = new System.Drawing.Size(799, 460);
             this.list_LOG.TabIndex = 7;
             // 
             // Server_LOG
             // 
             this.Server_LOG.AutoSize = true;
             this.Server_LOG.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Server_LOG.Location = new System.Drawing.Point(145, 185);
+            this.Server_LOG.Location = new System.Drawing.Point(314, 370);
+            this.Server_LOG.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.Server_LOG.Name = "Server_LOG";
-            this.Server_LOG.Size = new System.Drawing.Size(97, 19);
+            this.Server_LOG.Size = new System.Drawing.Size(195, 37);
             this.Server_LOG.TabIndex = 8;
             this.Server_LOG.Text = "Server LOG";
             // 
             // list_Recv
             // 
             this.list_Recv.FormattingEnabled = true;
-            this.list_Recv.ItemHeight = 12;
-            this.list_Recv.Location = new System.Drawing.Point(404, 207);
+            this.list_Recv.ItemHeight = 24;
+            this.list_Recv.Location = new System.Drawing.Point(875, 414);
+            this.list_Recv.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.list_Recv.Name = "list_Recv";
-            this.list_Recv.Size = new System.Drawing.Size(371, 232);
+            this.list_Recv.Size = new System.Drawing.Size(799, 460);
             this.list_Recv.TabIndex = 9;
             // 
             // Recv
             // 
             this.Recv.AutoSize = true;
             this.Recv.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Recv.Location = new System.Drawing.Point(540, 185);
+            this.Recv.Location = new System.Drawing.Point(1170, 370);
+            this.Recv.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.Recv.Name = "Recv";
-            this.Recv.Size = new System.Drawing.Size(152, 19);
+            this.Recv.Size = new System.Drawing.Size(302, 37);
             this.Recv.TabIndex = 10;
             this.Recv.Text = "Received Message";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1733, 900);
             this.Controls.Add(this.Recv);
             this.Controls.Add(this.list_Recv);
             this.Controls.Add(this.Server_LOG);
@@ -159,8 +171,10 @@
             this.Controls.Add(this.btn_EXIT);
             this.Controls.Add(this.btn_GameStart);
             this.Controls.Add(this.btn_ServerStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Bomb Game Server";
             this.ResumeLayout(false);
             this.PerformLayout();
 
